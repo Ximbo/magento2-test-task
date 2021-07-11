085a0a4dd8ce441f9f1e4167b6ea2cbf7a193a44
Ximbo <mail@ximbo.ru>
2021-07-11 19:03:28 +0300

    Index builder contract

---
+++ Api/IndexBuilderInterface.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @copyright: Copyright © 2021 Acme Company Inc. All rights reserved.
+ * @author   : Acme Company <mail@example.com>
+ */
+
+namespace Acme\Intro\Api;
+
+/**
+ * Interface IndexBuilderInterface
+ * @package Acme\Intro\Api
+ */
+interface IndexBuilderInterface
+{
+    /**
+     * Reindex all
+     *
+     * @return void
+     */
+    public function reindexAll();
+
+    /**
+     * Reindex partial indexation by ID list
+     *
+     * @param int[] $ids
+     * @return void
+     */
+    public function reindexList(array $ids);
+
+    /**
+     * Reindex partial indexation by ID
+     *
+     * @param int $id
+     * @return void
+     */
+    public function reindex(int $id);
+}
---
+++ Model/Indexer/IndexBuilder.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @copyright: Copyright © 2021 Acme Company Inc. All rights reserved.
+ * @author   : Acme Company <mail@example.com>
+ */
+
+namespace Acme\Intro\Model\Indexer;
+
+use Acme\Intro\Api\IndexBuilderInterface;
+
+/**
+ * Class IndexBuilder
+ * @package Acme\Intro\Model\Indexer
+ */
+class IndexBuilder implements IndexBuilderInterface
+{
+    /**
+     * @inheirtDoc
+     */
+    public function reindexAll()
+    {
+        // TODO: Implement reindexAll() method.
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function reindexList(array $ids)
+    {
+        // TODO: Implement reindexList() method.
+    }
+
+    /**
+     * @inheirtDoc
+     */
+    public function reindex(int $id)
+    {
+        // TODO: Implement reindexById() method.
+    }
+}
